178cbf1980d951f2de8a68ad97c855bf845bb562
Lam Pham <phamlam92@gmail.com>
2023-06-08 20:13:58 -0700

    Add(TRL-4): projectList interface

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -2,115 +2,124 @@ export const skillList = [
   {
     id: 1,
     name: "HTML",
-    image: "https://img.icons8.com/color/512/html-5.png"
+    image: "https://img.icons8.com/color/512/html-5.png",
   },
   {
     id: 2,
     name: "CSS",
-    image: "https://img.icons8.com/color/512/css3.png"
+    image: "https://img.icons8.com/color/512/css3.png",
   },
   {
     id: 3,
     name: "JavaScript",
-    image: "https://img.icons8.com/color/512/javascript--v1.png"
+    image: "https://img.icons8.com/color/512/javascript--v1.png",
   },
   {
     id: 4,
     name: "React",
-    image: "https://img.icons8.com/color/512/react-native.png"
+    image: "https://img.icons8.com/color/512/react-native.png",
   },
   {
     id: 5,
     name: "NodeJS",
-    image: "https://img.icons8.com/fluency/512/node-js.png"
+    image: "https://img.icons8.com/fluency/512/node-js.png",
   },
   {
     id: 6,
     name: "SQL",
-    image: "https://img.icons8.com/color/512/sql.png"
+    image: "https://img.icons8.com/color/512/sql.png",
   },
   {
     id: 7,
     name: "Bootstrap",
-    image: "https://img.icons8.com/color/512/bootstrap.png"
+    image: "https://img.icons8.com/color/512/bootstrap.png",
   },
   {
     id: 8,
     name: "Tailwind CSS",
-    image: "https://img.icons8.com/color/512/tailwindcss.png"
+    image: "https://img.icons8.com/color/512/tailwindcss.png",
   },
   {
     id: 9,
     name: "Git",
-    image: "https://img.icons8.com/color/512/git.png"
+    image: "https://img.icons8.com/color/512/git.png",
   },
   {
     id: 10,
     name: "Firebase",
-    image: "https://img.icons8.com/color/512/firebase.png"
+    image: "https://img.icons8.com/color/512/firebase.png",
   },
   {
     id: 11,
     name: "Heroku",
-    image: "https://img.icons8.com/color/512/heroku.png"
+    image: "https://img.icons8.com/color/512/heroku.png",
   },
   {
     id: 12,
     name: "Figma",
-    image: "https://img.icons8.com/color/512/figma.png"
-  }
+    image: "https://img.icons8.com/color/512/figma.png",
+  },
 ];
 
+import { StaticImageData } from "next/image";
 import {
   bestEatsImages,
   chatRoomImages,
   finessflixImages,
   goodSongsImages,
   netflixImages,
-  weatherImages
+  weatherImages,
 } from "./img";
 
-export const projectList = [
+interface IProjectListItem {
+  id: number;
+  name: string;
+  images: StaticImageData[];
+  features: string[];
+  description: string;
+}
+
+export const projectList: IProjectListItem[] = [
   {
     id: 1,
     name: "Finessflix",
     images: finessflixImages,
     features: ["React", "CSS", "NodeJS", "PostgreSQL"],
-    description: "Enter description here"
+    description: "Enter description here",
   },
   {
     id: 2,
     name: "Best Eats",
     images: bestEatsImages,
     features: ["React", "Tailwind CSS", "Responsive", "Filtering"],
-    description: "Enter description here"
+    description: "Enter description here",
   },
   {
     id: 3,
     name: "Chat Room",
     images: chatRoomImages,
     features: ["React", "Tailwind CSS", "Firebase"],
-    description: "Enter description here"
+    description: "Enter description here",
   },
   {
     id: 4,
     name: "Netflix Clone",
     images: netflixImages,
     features: ["React", "Tailwind CSS", "Firebase", "The Movie DB API"],
-    description: "Enter description here"
+    description: "Enter description here",
   },
   {
     id: 5,
     name: "Weather App",
     images: weatherImages,
     features: ["React", "CSS", "OpenWeatherMap API"],
-    description: "Enter description here"
+    description: "Enter description here",
   },
   {
     id: 6,
     name: "Good Songs Growing Up",
     images: goodSongsImages,
     features: ["HTML", "CSS", "JavaScript", "NodeJS"],
-    description: "Enter description here"
-  }
+    description: "Enter description here",
+  },
 ];
